69059aa3a87c2955abfac31dc3bd5fe531a6102d
AusJad <jarad53@hotmail.com>
2018-05-15 07:35:31 +0800

    camera stutters
    needed to push this so at least its there.

--- Resources/Scripts/startup.lua
+++ Resources/Scripts/startup.lua
@@ -45,7 +45,7 @@ local function loadResources(AMAN)
 
 
 	AMAN:addModel("./Resources/Models/Rock.obj", "IM", "ROCK", vec3(15,15,15));
-	AMAN:addModel("./Resources/Models/lava.obj", "IM", "LAVA", vec3(50,50,50));
+	AMAN:addModel("./Resources/Models/lava.obj", "IM", "LAVA", vec3(1,1,1));
 	AMAN:addModel("./Resources/Models/bullet.obj", "IM", "bullet", vec3(1,1,1));
 	AMAN:addModel("./Resources/Models/robotgreen.obj", "IM", "ROBOT", vec3(0.5,0.5,0.5));
 	AMAN:addModel("./Resources/Models/robotgreen.obj", "IM", "Robot2", vec3(0.3,0.3,0.3));
@@ -100,42 +100,8 @@ function initGame(SM, LSM, AMAN, AE)
 	--Initalise Level 1
 	SM:addScene();
 	SM:setCurrScene(level1);
-<<<<<<< HEAD
-	SM:setSceneResources(ResourceList("updatefunc", "level1Update", "renderfunc", "level1Render"), level1);
 
-	SM:attachControls(level1, ResourceList("keyCallback", "keys", "mouseCallback", "mouse"));
-	SM:attachTerrain(Identifiers("TO", "Terrain"), level1, vec3(0,0,0), ResourceList("model", "Terrain"));
-	
-	--Adding game objects
-	for i = 11,1,-1 
-	do 
-		id = tostring(math.random())
-		SM:addObject(Identifiers("SE"), level1, vec3(math.random (-128*40, 128*40), 0, math.random(-128*40, 128*40)), ResourceList("model", "ROCK")); 
-		SM:addObject(Identifiers("NPC", id), level1, vec3(math.random(-128*40, 128*40), 0, math.random(-128*40, 128*40)), ResourceList("model", "PLAYERW", "updatefunc", "start", "msgrcvr", "msgrcvr"));
-	end
-	SM:addObject(Identifiers("CAM","Camera"), level1, vec3(0, 0, -4000), ResourceList());
-	SM:addObject(Identifiers("PROP", "SKYBOX"), level1, vec3(0,-5000,0), ResourceList("model", "SKYBOX"));
-	
-	--SM:addObject(Identifiers("PROP", "LAVA"), level1, vec3(0,100,0), ResourceList("model", "LAVA"));
-	
-	--SM:addObject(Identifiers("MO","Guide"), level1, vec3(0, 0, 0), ResourceList("model", "popup"));
-	--SM:addObject(Identifiers("NPC", "R1"), level1, vec3(-80,0,0), ResourceList("model", "ROBOT", "updatefunc", "start", "msgrcvr", "msgrcvr"));
-	--SM:addObject(Identifiers("NPC", "R2"), level1, vec3(480,0,-1000), ResourceList("model", "ROBOT"));
-	SM:addObject(Identifiers("PROP", "SHIP"), level1, vec3(-1000,1500,2000), ResourceList("model", "SHIP"));
-	SM:addObject(Identifiers("PROP", "SHIP2"), level1, vec3(1000,2000,-2000), ResourceList("model", "SHIP"));
-
-	SM:addObject(Identifiers("PLYR", "Player"), level1, vec3(0, 0, -4000), ResourceList("camera", "Camera", "msgrcvr", "playerMsgRcvr", "renderfunc", "playerHUDRenderer"));
-	
-	--Set height map
-	SM:setSceneHeightMap(level1, SM:GetGameObject("Terrain"));
-
-	--Setup sound
-	AE:setListenerSource(SM:GetGameObjectID("Camera"), vec3(0, 0, 0));
-	AE:playSoundatSource("S1MUSIC", SM:GetGameObjectID("Camera"), vec3(0, 0, 0));
-	AE:playSoundatSource("HIGHGROUND", SM:GetGameObjectID("Camera"), vec3(0, 0, 0));
-=======
 	initLvl1(SM, AE);
->>>>>>> master
 
 	--Initalise Level 2
 	SM:addScene();
